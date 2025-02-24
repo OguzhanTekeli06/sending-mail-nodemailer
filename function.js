558550062c21aafdb7ac05ad312769ce875795df
Oguzhantekeli06 <213301042@ogr.selcuk.edu.tr>
2025-02-24 11:45:50 +0300

    some improvements

--- function.js
+++ function.js
@@ -1,7 +1,9 @@
 const nodemailer = require("nodemailer");
 require('dotenv').config();
-async function sendEmail(fromEmail,toEmail,subject,html) {
 
+console.log(process.env.EMAIL_USER,process.env.EMAIL_PASS);
+
+async function sendEmail(toEmail,subject,html) {            // hep aynı mail ile yollanacak ise fromEmail paramatresine gerk yok.gerek olsaydı from kısmına eklenirdi.
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
@@ -9,11 +11,11 @@ async function sendEmail(fromEmail,toEmail,subject,html) {
             pass: process.env.EMAIL_PASS
         },
         secure: true,
-        port: 465,
-        host: 'smtp.gmail.com'
+        port: 465,          //tls üzerinden doğrudan bağlantı içindir    //alternatif 587 ve secure false.
+        host: 'smtp.gmail.com'         
     });
     try{
-        const info = await tranporter.sendMail({         
+        const info = await transporter.sendMail({         
             from: process.env.EMAIL_USER,            // bizim mail
             to: toEmail,                                   //hedef mail ya da mailler
             subject: subject,
@@ -25,13 +27,12 @@ async function sendEmail(fromEmail,toEmail,subject,html) {
         console.error("mail gönderilemedi",error)
     };
 }
-// const html = ` 
-//     <h1>html formatında da yollanabilir</h1>
-// `;
-// const fromEmail = process.env.EMAIL_USER
-// const toEmail = 'oguz.tekeli.37@gmail.com'
-// const subject = 'Hello'
-sendEmail(fromEmail,toEmail,subject,html);
+ const html = ` 
+     <h1>html formatında da yollanabilir</h1>
+ `;
+const toEmail = 'oguz.tekeli.37@gmail.com'
+const subject = 'Hello'
+sendEmail(toEmail,subject,html);   
 // const emails = [aaa,aaa,aaa]
 // to: emails, ile toplu mail atılabilir
 
